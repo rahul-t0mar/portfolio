06eb07bfbd3141e04af51233df49a52e425031a2
Rahul <rahultomarjobs6296@gmail.com>
2026-07-24 20:56:50 +0530

    Add links to the icons

--- src/Components/ConnectIcons.jsx
+++ src/Components/ConnectIcons.jsx
@@ -7,16 +7,16 @@ import styles from "./ConnectIcons.module.css";
 function ConnectIcons() {
   return (
     <div className={`${styles.connect_icons}`}>
-      <a href="#">
+      <a href="https://github.com/rahul-t0mar">
         <GitHubIcon className={`${styles.icon}`} />
       </a>
       <a href="#">
         <XIcon className={`${styles.icon}`} />
       </a>
-      <a href="#">
+      <a href="https://www.linkedin.com/in/rahul-t0mar-dzire045/">
         <LinkedInIcon className={`${styles.icon}`} />
       </a>
-      <a href="#">
+      <a href="https://www.instagram.com/rahul.t0mar/">
         <InstagramIcon className={`${styles.icon}`} />
       </a>
     </div>
